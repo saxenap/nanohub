20de21d7565a2de8ff4c8fceb01238cfc195cf00
Xufeng Wang <wang159@purdue.edu>
2019-12-06 00:38:34 -0500

    forgot to add file

--- clustering/main_online_users_TS_analysis.py
+++ clustering/main_online_users_TS_analysis.py
@@ -1,31 +1,66 @@
 import argparse
-from pprint import pprint
+from pprint import pprint, pformat
 import code
 import os
 
-from clustering_cores.core_classroom_analysis.core_classroom_analysis import core_classroom_analysis
+import logging
+
+from preprocessing.gather_data import gather_data
+from core_classroom_detection.core_classroom_analysis import core_classroom_analysis
 
 def main_online_users_TS_analysis():
+
     parser = argparse.ArgumentParser(description='Online user weblog time series analysis originally designed for nanoHUB.org')
     
-    ### (OPTIONAL)
+    # SQL connection
+    parser.add_argument('--SQL_username', help='SQL database username', 
+                                         action='store', default='invalid SQL username')
+    parser.add_argument('--SQL_password', help='SQL password', 
+                                         action='store', default='invalid SQL password')
+    parser.add_argument('--SQL_addr', help='SQL address', 
+                                         action='store', default='127.0.0.1')
+    parser.add_argument('--SQL_port', help='SQL port', 
+                                         action='store', default='3306')
+                                                                                                                       
+    # directories
     parser.add_argument('--scratch_dir', help='location of scratch directory for temporary files', 
                                          action='store', default=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'temp'))
                                     
-    ### internal options
+    # internal options
     parser.add_argument('--CI', help='start GitLab CI pipeline', 
                                 action='store_true')
     parser.add_argument('--CI_dir', help='location of CI directory', 
                                     action='store', default=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'CI'))
-                                    
+               
     inparams = parser.parse_args()       
+
     
-    ### TODO summarize input options
-    pprint(inparams)
+    # TODO summarize input options
+    if inparams.CI:
+        # CI/Test runs
+        # The only difference here should be CI/Test runs use sample, 
+        # cleaned data instead of live SQL data
+        logging.basicConfig(level=logging.DEBUG, format='%(message)s')
+        
+        logging.info('GitLab CI runs')
+        
+    else:
+        # Production runs
+        logging.basicConfig(level=logging.INFO, format='%(message)s')
+        pass
+        
+    display_inparams = inparams
+    if 'SQL_password' in display_inparams:
+        display_inparams.SQL_password = ['*' for x in display_inparams.SQL_password]
+        
+    logging.info(pformat(vars(inparams)))
     
-    ### Analysis: classroom detection
+    #
+    # Analysis: classroom detection
+    #
                     
     # prepare data
+    gather_data(inparams)
     
     # classroom detection
     core_classroom_analysis(inparams)
