71cf61fac871bdfc8d2803cb3c799722c3145c2a
henry <wang2506@purdue.edu>
2021-08-05 18:40:40 -0400

    updated plots for geo users, and classroom + semester plots

---
+++ nanoHUB/pipeline/additional_imports/temp_upload.py
@@ -0,0 +1,15 @@
+# Parameters
+api_url = '/services/data/v43.0/sobjects'
+
+# login parameters to be handled by Papermill
+sql_login_params = {"username": "wang2506_ro", "password": "fnVnwcCS7iT45EsA"}
+sf_login_params = {
+    "grant_type": "password",
+    "client_id": "3MVG95jctIhbyCppj0SNJ75IsZ1y8UPGZtSNF4j8FNVXz.De8Lu4jHm3rjRosAtsHy6qjHx3i4S_QbQzvBePG",
+    "client_secret": "D1623C6D3607D4FC8004B92C761DFB6C1F70CCD129C5501E357028DFA00F5764",
+    "username":"wang2506@purdue.edu",
+    "password":"purdue99272844vp8Z6qq0teuPQmirGaaPAt4", 
+}
+    #"username": "wang159-4j1v@force.com",
+    #"password": "napoleon0eZ3PQpQqE3C3z4wWqEqKGhQ8",
+    
