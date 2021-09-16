d4ccc44e42557489d410b4afdd20cb25affa5442
Praveen Saxena <saxep01@gmail.com>
2021-09-16 14:42:56 -0400

    add ability to call any task in Makefile and logging

--- tasks.mk
+++ tasks.mk
@@ -1,6 +1,39 @@
 #!make
+THIS_FILE := $(lastword $(MAKEFILE_LIST))
+
 ROOT_DIR=$(shell dirname $(realpath $(firstword $(MAKEFILE_LIST))))
-SALESFORCE_DIR=$(ROOT_DIR)/nanoHUB/pipeline/salesforce
+NANOHUB_DIR=$(ROOT_DIR)/nanoHUB
+PIPELINE_DIR=$(NANOHUB_DIR)/pipeline
+SALESFORCE_DIR=$(PIPELINE_DIR)/salesforce
+
+EXECUTE_TASK=python3 $(ROOT_DIR)/nanoHUB/__main__.py task execute
+log-level=INFO
+logger=--log-level=$(log-level) 2>&1 | /usr/bin/logger -t PIPELINE
+
+TASKS=$(SALESFORCE_DIR)/_task_test.ipynb \
+	$(SALESFORCE_DIR)/task_citations.ipynb  \
+	$(SALESFORCE_DIR)/task_citations_map_contacts.ipynb  \
+	$(SALESFORCE_DIR)/task_citations_map_leads.ipynb   \
+	$(SALESFORCE_DIR)/task_determine_contact_cluster_org.ipynb  \
+	$(SALESFORCE_DIR)/task_issue_url.ipynb  \
+	$(SALESFORCE_DIR)/task_organization.ipynb  \
+	$(SALESFORCE_DIR)/task_orgs_map_contacts.ipynb  \
+	$(SALESFORCE_DIR)/task_tool_basic_updates.ipynb  \
+	$(SALESFORCE_DIR)/task_tools_map_authors.ipynb   \
+	$(SALESFORCE_DIR)/task_tools_map_contacts.ipynb  \
+	$(SALESFORCE_DIR)/task_user_basic_updates.ipynb  \
+	$(SALESFORCE_DIR)/delete_spam_users.ipynb  \
+	$(SALESFORCE_DIR)/task_update_email_preferences.ipynb  \
+	$(SALESFORCE_DIR)/task_nh_survey_urls.ipynb  \
+	$(SALESFORCE_DIR)/task_user_org_reclass.ipynb  \
+	$(SALESFORCE_DIR)/task_update_static_personalinfo.ipynb  \
+	$(SALESFORCE_DIR)/db2users_remap_sf.ipynb  \
+	$(SALESFORCE_DIR)/task_nh_usage_survey.ipynb  \
+	$(SALESFORCE_DIR)/task_user_contribution_update.ipynb  \
+	$(PIPELINE_DIR)/researcher_scraping/main_file.py  \
+	$(PIPELINE_DIR)/SF_dataimports/general_imports.ipynb
+
+.PHONY: task
 
 env_vars:
 	env
@@ -8,32 +41,8 @@ env_vars:
 heartbeat:
 	/usr/bin/logger -t PIPELINE -p user.info "Heart Beat Check."
 
-test:
-	python3 $(ROOT_DIR)/nanoHUB/__main__.py task execute \
-		$(SALESFORCE_DIR)/_task_test.ipynb \
-	--log-level=INFO 2>&1 | /usr/bin/logger -t PIPELINE
-
 execute:
-	python3 $(ROOT_DIR)/nanoHUB/__main__.py task execute \
-		$(SALESFORCE_DIR)/_task_test.ipynb  \
-		$(SALESFORCE_DIR)/task_citations.ipynb  \
-		$(SALESFORCE_DIR)/task_citations_map_leads.ipynb   \
-		$(SALESFORCE_DIR)/task_determine_contact_cluster_org.ipynb  \
-		$(SALESFORCE_DIR)/task_issue_url.ipynb  \
-		$(SALESFORCE_DIR)/task_organization.ipynb  \
-		$(SALESFORCE_DIR)/task_orgs_map_contacts.ipynb  \
-		$(SALESFORCE_DIR)/task_tool_basic_updates.ipynb  \
-		$(SALESFORCE_DIR)/task_tools_map_authors.ipynb   \
-		$(SALESFORCE_DIR)/task_tools_map_contacts.ipynb  \
-		$(SALESFORCE_DIR)/task_user_basic_updates.ipynb  \
-		$(SALESFORCE_DIR)/delete_spam_users.ipynb  \
-		$(SALESFORCE_DIR)/task_update_email_preferences.ipynb  \
-		$(SALESFORCE_DIR)/task_nh_survey_urls.ipynb  \
-		$(SALESFORCE_DIR)/task_user_org_reclass.ipynb  \
-		$(SALESFORCE_DIR)/task_update_static_personalinfo.ipynb  \
-		$(SALESFORCE_DIR)/db2users_remap_sf.ipynb  \
-		$(SALESFORCE_DIR)/task_nh_usage_survey.ipynb  \
-		$(SALESFORCE_DIR)/task_user_contribution_update.ipynb  \
-		nanoHUB/pipeline/researcher_scraping/main_file.py  \
-		nanoHUB/pipeline/SF_dataimports/general_imports.ipynb \
-	--log-level=INFO 2>&1 | /usr/bin/logger -t PIPELINE
+	$(EXECUTE_TASK) $(TASKS) $(logger)
+
+test:
+	$(MAKE) -f $(THIS_FILE) execute TASKS=$(SALESFORCE_DIR)/_task_test.ipynb
