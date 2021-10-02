08392b51bf3b9281f1572cbac893fd4045233b86
dyshang <dingyanshang@gmail.com>
2021-10-02 18:02:13 -0400

    transform raindrop to a callable fn

--- nanoHUB/class-cluster/cluster_overlap/cluster_overlap.py
+++ nanoHUB/class-cluster/cluster_overlap/cluster_overlap.py
@@ -1,3 +1,5 @@
+from visualization_V2.MakeFullRaindrop import call_func
+
 def get_cluster_overlap(year, semester):
     if semester == "f":
         semester_full = "fall"
@@ -33,10 +35,8 @@ def get_cluster_overlap(year, semester):
             m_id += 1
 
         overlap_list.sort(key=lambda x: (x[0], -x[3]))
-        print(overlap_list)
-        with open('cluster_overlap.csv', 'a') as f:
-            writer = csv.writer(f)
-            writer.writerows(overlap_list)
+        # print(overlap_list)
+        return overlap_list
 
 
 if __name__ == '__main__':
@@ -48,7 +48,23 @@ if __name__ == '__main__':
         writer = csv.writer(f)
         writer.writerow(header)
 
+    geofilename = "/Users/sdy/Desktop/nanoHUB/clustercodes/users_geoclustered_protocol_5_6_7_on_or_after_2007-01-01.20131231.csv"
+
     for i in range(2007, 2008):
-        for s in ["s", "f"]:
+        for s in ["s"]:
+        # for s in ["s", "f"]:
             if i != 2021 or s != "f":
-                get_cluster_overlap(i, s)
+                overlap_list = get_cluster_overlap(i, s)
+
+                # combine xufeng clusters with the same MClusterID
+                x_combined = [set() for _ in range(max([x[0] for x in overlap_list]))]
+                for l in overlap_list:
+                    x_combined[l[0] - 1].update(l[9])
+
+                with open('cluster_overlap.csv', 'a') as f:
+                    writer = csv.writer(f)
+                    writer.writerows(overlap_list)
+                startdate = str(i) + ('-1' if s == 's' else '-7') + '-1'
+                enddate = str(i) + '-6-30' if s == 's' else '-12-31'
+                call_func(startdate, enddate, geofilename, [overlap_list[0][7],overlap_list[0][5],overlap_list[0][9]])
+                # call_func(startdate, enddate, geofilename, [list(x) for x in x_combined])
