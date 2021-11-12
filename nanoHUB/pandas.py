47a7e8e9b6139dcd4740fddb563cb6d50a0669d1
Praveen Saxena <saxep01@gmail.com>
2021-11-12 12:21:43 +0530

    change module name

--- nanoHUB/pandas.py
+++ nanoHUB/pandas.py
@@ -1,14 +1,14 @@
-import pandas
+import pandas as pd
 from IPython.display import display, Markdown
 
 
-def get_rows_by_keyvalue(df: pandas.DataFrame, key_name: str, search_value) -> pandas.DataFrame:
+def get_rows_by_keyvalue(df: pd.DataFrame, key_name: str, search_value) -> pd.DataFrame:
     return df.loc[df[key_name] == search_value]
 
 
-def get_number_of_rows(df: pandas.DataFrame) -> int:
+def get_number_of_rows(df: pd.DataFrame) -> int:
     return len(df.index)
 
 
-def display_number_of_rows(df: pandas.DataFrame) -> None:
+def display_number_of_rows(df: pd.DataFrame) -> None:
     display(Markdown("**A total of %d entries pulled in**" % get_number_of_rows(df)))
