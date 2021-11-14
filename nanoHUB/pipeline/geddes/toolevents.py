fd07aa11e61402d47394c9be46765aa7c1c0f0c1
Praveen Saxena <saxep01@gmail.com>
2021-11-14 22:24:33 +0530

    add data access

--- nanoHUB/pipeline/geddes/toolevents.py
+++ nanoHUB/pipeline/geddes/toolevents.py
@@ -15,7 +15,7 @@ query = QueryString(
     'start'
 )
 bucket_name = 'nanohub.raw'
-from_date = datetime.date(2012,12,25)
+from_date = datetime.date(2012,1,1)
 end = datetime.date.today()
 
 while from_date < end:
