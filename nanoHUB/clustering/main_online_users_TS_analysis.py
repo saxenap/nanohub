33e9068842f8814501b0057249b5f43c57b67eb2
Zachary Hunter <hunte164@purdue.edu>
2022-06-20 10:43:47 -0400

    reconstructing flags/parsing of clustering to be easily callable

--- nanoHUB/clustering/main_online_users_TS_analysis.py
+++ nanoHUB/clustering/main_online_users_TS_analysis.py
@@ -18,27 +18,20 @@ from save_clusters_to_geddes import save_clusters_to_geddes
 
 #how would you have done it?
 
-def main_online_users_TS_analysis(task, firstyear, lastyear, bucket_name, object_path, save_to_geddes: bool, use_old_data: bool, gather_data_only: bool) -> pd.DataFrame:
-
-    firstyear = firstyear + "-01-01"  # semester 1
-    lastyear = lastyear + "-07-02"  # semester 2
-    class_probe_range = firstyear + ":" + lastyear
-    if bucket_name == '':
-        bucket_name = 'nanohub.processed'
-    if object_path == '':
-        object_path = 'clusters/${' + task + '}/by_semester'
-
+def main_online_users_TS_analysis(flags) -> pd.DataFrame:
+    print(flags.firstYear)
+    # class_probe_range = flags.firstYear + ':' + flags.lastYear
     #
     # Analysis:
     #
 
     #task selection
     ###
-    if task == 'classroom_detection' or task == 'xufeng':
+    if flags.task == 'classroom_detection' or flags.task == 'xufeng':
         # classroom detection
         func = core_classroom_analysis
 
-    elif task == 'cost_cluster_analysis' or task == 'mike':
+    elif flags.task == 'cost_cluster_analysis' or flags.task == 'mike':
         # quick cost-function clustering analysis
 
         func = core_cost_cluster_analysis
@@ -50,33 +43,33 @@ def main_online_users_TS_analysis(task, firstyear, lastyear, bucket_name, object
     # logging.basicConfig(level=numeric_level, format='%(message)s')
     #     # summarize input options
 
-    # #cleaned data/live data
-    # if inparams.CI:
-    #     # CI/Test runs
-    #     # The only difference here should be CI/Test runs use sample,
-    #     # cleaned data instead of live SQL data
-    #
-    #
-    #     logging.info('GitLab CI runs')
-    #
-    #     # setting the default time range for CI
-    #     inparams.class_probe_range = '2018-1-1:2018-5-1'
-    #     logging.info('Setting analysis time range to CI default: ' + inparams.class_probe_range)
-    # #cleaned data/live data
+    #cleaned data/live data
+    if flags.CI:
+        # CI/Test runs
+        # The only difference here should be CI/Test runs use sample,
+        # cleaned data instead of live SQL data
+
+
+        logging.info('GitLab CI runs')
+
+        # setting the default time range for CI
+        flags.class_probe_range = '2018-1-1:2018-5-1'
+        logging.info('Setting analysis time range to CI default: ' + flags.class_probe_range)
+    #cleaned data/live data
 
     #timeframe
-    # if inparams.class_probe_range == 'latest':
-    #     # probes only the latest (today - 2 STD of Gaussian attention window function)
-    #     # Each user simulation run action is expanded to 1 STD, and therefore the resulting cluster has max width of 2 STD
-    #     inparams.data_probe_range = [datetime.date.today() - datetime.timedelta(days=inparams.class_attention_span * 2),
-    #                                  datetime.date.today()]
-    #     inparams.class_probe_range = [x.strftime("%Y-%m-%d") for x in inparams.data_probe_range]
+    if flags.class_probe_range == 'latest':
+        # probes only the latest (today - 2 STD of Gaussian attention window function)
+        # Each user simulation run action is expanded to 1 STD, and therefore the resulting cluster has max width of 2 STD
+        flags.data_probe_range = [datetime.date.today() - datetime.timedelta(days=flags.class_attention_span * 2),
+                                     datetime.date.today()]
+        flags.class_probe_range = [x.strftime("%Y-%m-%d") for x in flags.data_probe_range]
 
-    # else:
+    else:
         # probes given time range
         # expects inparams.class_probe_range in form of, for example, '2018-1-1:2018-5-1'
-    class_probe_range = class_probe_range.split(':')
-    data_probe_range = [datetime.datetime.strptime(x, '%Y-%m-%d') for x in class_probe_range]
+        class_probe_range = flags.class_probe_range.split(':')
+        data_probe_range = [datetime.datetime.strptime(x, '%Y-%m-%d') for x in class_probe_range]
     #timeframe
 
     #moved above 2 lines out of else:
@@ -86,38 +79,38 @@ def main_online_users_TS_analysis(task, firstyear, lastyear, bucket_name, object
     #
 
     #checks to see if bucket/path are valid
-    if save_to_geddes:
-        if bucket_name is None or bucket_name == '':
+    if flags.save_to_geddes:
+        if flags.bucketName is None or flags.bucketName == '':
             raise ValueError("A bucket name is necessary in order to save results to Geddes")
-        if object_path is None or object_path == '':
+        if flags.objectPath is None or flags.objectPath == '':
             raise ValueError("An object path is necessary in order to save results to Geddes")
     # checks to see if bucket/path are valid
 
     # # create scratch directory if it does not exist
-    # if not os.path.exists(get_scratch_dir(inparams)):
-    #     logging.info('Creating new scratch directory: ' + get_scratch_dir(inparams))
-    #     os.mkdir(get_scratch_dir(inparams))
-    #
-    # if inparams.no_save_output:
-    #     logging.info("Skipping saving output locally ...")
-    #
-    # if not inparams.save_to_geddes:
-    #     logging.info("Skipping saving output to Geddes ...")
+    if not os.path.exists(get_scratch_dir(flags)):
+        logging.info('Creating new scratch directory: ' + get_scratch_dir(flags))
+        os.mkdir(get_scratch_dir(flags))
 
-    if not use_old_data:
+    if flags.noSaveOutput:
+        logging.info("Skipping saving output locally ...")
+
+    if not flags.saveToGeddes:
+        logging.info("Skipping saving output to Geddes ...")
+
+    if not flags.useOldData:
         # logging.info('Gathering data  ......')
-        gather_data(inparams) #unsure how to handle with current solution
-        if gather_data_only:
+        gather_data(flags) #unsure how to handle with current solution
+        if flags.gatherDataOnly:
             return
-    # else:
-    #     logging.info('Option "--user_old_data" enabled. Using data from previous run ......')
+    else:
+        logging.info('Option "--user_old_data" enabled. Using data from previous run ......')
 
-    # logging.debug(pformat(vars(inparams)))
+    logging.debug(pformat(vars(flags)))
 
-    final_clusters_df = func(inparams) #runs clustering
+    final_clusters_df = func(flags) #runs clustering
 
-    if save_to_geddes == True:
-        save_clusters_to_geddes(final_clusters_df, bucket_name, class_probe_range, object_path)
+    if flags.saveToGeddes == True:
+        save_clusters_to_geddes(final_clusters_df, flags.bucketName, flags.class_probe_range, flags.objectPath)
 
     return final_clusters_df
 
--- nanoHUB/clustering/run_clustering.py
+++ nanoHUB/clustering/run_clustering.py
@@ -1,24 +1,57 @@
 import os
-from main_online_users_TS import main_online_users_TS
+from dataclasses import dataclass, field
+from main_online_users_TS_analysis import main_online_users_TS_analysis
 
-def run_clustering(task, firstyear, lastyear, bucketname, objectpath, loglevel):
-    firstyear = firstyear + "-01-01" #semester 1
-    lastyear = lastyear + "-07-02" #semester 2
-    if bucketname == '':
-        bucketname = 'nanohub.processed'
-    if objectpath == '':
-        objectpath = 'clusters/${' + task + '}/by_semester'
-    if loglevel == '':
-        loglevel = 'INFO'
+@dataclass
+class ClusteringFlags:
+    #general
+    task: str
+    firstYear: str
+    lastYear: str
+    class_probe_range: str = field(default = firstYear + ":" + lastYear) #check
 
-    cmd1 = os.system("python3 main_online_users_TS.py "
-                     "--cost_probe_range " + firstyear + ":" + lastyear + " "
-                     "--class_probe_range " + firstyear + ":" + lastyear + " "
-                     "--task $(" + task + ") "
-                     "--bucket_name $(" + bucketname + ") "
-                     "--object_path $(" + objectpath + ") "
-                     "--log_level $(" + loglevel + ") "
-                     "(other_flags)")
+    #data
+    geoip2_mmdb_filepath: str = field(default=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data', 'GeoLite2-City.mmdb')) #check
+
+    #directories
+    outputDir: str = field(default = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'output')) #check
+    scratchDir: str = field(default=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'temp'))  #check
+    namePrefix: str = field(default='users_analysis')
+    displayOutput: bool = field(default='True')
+    noSaveOutput: bool = field(default='False')
+    saveToGeddes: bool = field(default='False')
+    bucketName: str = field(default='nanohub.processed')
+    objectPath: str = field(default='clusters/${' + task + '}/by_semester')
+
+    #classroom detection behavior (xufeng)
+    classActivityTol: str = field(default='2')
+    classAttentionSpan: str = field(default='3')
+    classSizeMin: str = field(default='5')
+    classDistanceThreshold: str = field(default ='50')
+    classMergeTimeThreshold: str = field(default='120')
+    classMergeDistanceThreshold: str = field(default='5')
+
+    #quick cost-based cluster analysis
+    costSizeMin: str = field(default='4')
+    costForceAllDiffLvl: str = field(default='501')
+    costTolerance: str = field(default='57')
+
+    #dask
+    daskScheduler: str = field(default='single-threaded')
+
+    #internal options
+    CI: str = field(default='False') #check
+    CI_dir: str = field(default=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'CI')) #FILL
+    generateNotebookCheckpoints: str = field(default='True')
+    gatherDataOnly: bool = field(default='False') #check
+    useOldData: bool = field(default='False') #check
+    logLevel: str = field(default='INFO')
+
+def run_clustering(flags):
+    flags.firstYear = flags.firstYear + "-01-01" #semester 1
+    flags.lastYear = flags.lastYear + "-07-02" #semester 2
+
+    main_online_users_TS_analysis(flags)
 
 
 
@@ -35,4 +68,4 @@ def run_clustering(task, firstyear, lastyear, bucketname, objectpath, loglevel):
 #     print(lol) #
 
 if __name__ == '__main__':
-    run_clustering('mike', '2006', '2006', '', '', '')
+    run_clustering(ClusteringFlags())
