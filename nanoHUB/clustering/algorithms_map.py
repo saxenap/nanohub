bee8205ea1b43bbe422f402558ba304968736d89
Praveen Saxena <saxep01@gmail.com>
2022-06-22 15:48:39 -0400

    review with Zach

--- nanoHUB/clustering/algorithms_map.py
+++ nanoHUB/clustering/algorithms_map.py
@@ -1,25 +1,28 @@
 from dataclasses import dataclass, asdict
 import pandas as pd
 import json
-import logging
+from dataclasses import dataclass, field
 from core_classroom_detection.core_classroom_analysis import core_classroom_analysis
 from core_quick_cluster_detection.core_cost_cluster_analysis import core_cost_cluster_analysis
 from save_clusters_to_geddes import save_clusters_to_geddes
 from preprocessing.gather_data import gather_data
 from pprint import pprint, pformat
+from datetime import datetime, date
+import logging
 
-class IPreExecutor:
-    def preexecute(flags) -> None:
+
+class IValidator:
+    def validate(self, command) -> None:
         raise NotImplementedError()
 
 
-class DateMapper(IPreExecutor):
-    def preexecute(flags) -> None:
-        if not self.class_probe_range:
-            self.class_probe_range = self.start_date + ":" + self.end_date
+class DateValidator(IValidator):
+    def validate(self, command) -> None:
+        if not command.class_probe_range:
+            command.class_probe_range = command.start_date + ":" + command.end_date
 
-        start_check = datetime.strptime(flags.start_date, '%Y-%m-%d')
-        end_check = datetime.strptime(flags.end_date, '%Y-%m-%d')
+        start_check = datetime.strptime(command.start_date, '%Y-%m-%d')
+        end_check = datetime.strptime(command.end_date, '%Y-%m-%d')
 
         if not start_check < end_check:
             raise Exception("start_date before end_date")
@@ -33,94 +36,115 @@ class AlgorithmsMap:
 
     mike: str = 'core_cost_cluster_analysis'
     cost_cluster_analysis: str = 'core_cost_cluster_analysis'
-    cost_cluster_analysis: str = 'cost_cluster_analysis'
-
+    core_cost_cluster_analysis: str = 'cost_cluster_analysis'
 
     def __str__(self) -> str:
         return json.dumps(asdict(self))
 
-class DataLoader(IPreExecutor):
-    def execute(flags) -> None:
-        if flags.use_old_data == False:
-            logging.info('Gathering data .....')
-            gather_data(flags)
-        else:
-            logging.info('Option "--user_old_data" enabled. Using data from previous run')
 
-        logging.debug(pformat(vars(flags)))
-
-class IAlgorithmExecutor:
-    def execute(flags) -> pd.DataFrame:
+class IExecuteAlgorithm:
+    def handle(self, command) -> pd.DataFrame:
         raise NotImplementedError()
 
-class AlgorithmExecutor(IAlgorithmExecutor):
-    def __init__(preexecutors: [], _map: AlgorithmsMap):
-        self.preexecutors = preexecuttors
+
+class ValidationHandler(IExecuteAlgorithm):
+    def __init__(self, validators: [], handler: IExecuteAlgorithm, logger: logging.Logger):
+        self.validators = validators
+        self.handler = handler
+        self.logger = logger
+
+    def handle(self, command) -> pd.DataFrame:
+        for validator in self.validators:
+            validator.validate(command)
+
+        return self.handler.handle(command)
+
+
+class AlgorithmHandler(IExecuteAlgorithm):
+    def __init__(self, _map: AlgorithmsMap, logger: logging.Logger):
         self._map = _map
+        self.logger = logger
 
-    def execute(flags) -> pd.DataFrame:
-        for preexecutor in self.preexecutors:
-            execute(flags)
+    def handle(self, command) -> pd.DataFrame:
+        if command.use_old_data == False:
+            logging.info('Gathering data .....')
+            gather_data(command)
+        else:
+            self.logger.info('Option "--user_old_data" enabled. Using data from previous run')
 
-            self.data_loader(flags)
-            if flags.gather_data_only:
-                return
-            if not hasattr(_map, flags.task)
-                raise ValueError("A task must be assigned.")
-            func = getattr(_map, flags.task)
-            return func(flags)
+        if command.gather_data_only:
+            self.logger.info("Only gathering data required => Returning empty dataframe ...")
+            return pd.DataFrame()
 
-class GeddesSaver(IAlgorithmExecutor):
-    def __init__(executor: IAlgorithmExecutor):
-        self.executor = executor
+        if  not command.task or not hasattr(self._map, command.task):
+            raise Exception(
+                "Invalid Algorithm/Task %s. A valid task must be assigned. Valid algorithms/tasks are: %s" % (command.task, self._map)
+            )
 
-    def execute(flags) -> pd.DataFrame:
-        if flags.save_to_geddes == True:
-            if flags.bucket_name is None or flags.bucket_name == '':
+        func = getattr(self._map, command.task)
+        return func(command)
+
+
+class GeddesSaver(IExecuteAlgorithm):
+    def __init__(self, handler: IExecuteAlgorithm, logger: logging.Logger):
+        self.handler = handler
+        self.logger = logger
+
+    def handle(self, command) -> pd.DataFrame:
+        if command.save_to_geddes == True:
+            if command.bucket_name is None or command.bucket_name == '':
                 raise ValueError("A bucket name is necessary in order to save results to Geddes")
-            if flags.object_path is None or flags.object_path == '':
+            if command.object_path is None or command.object_path == '':
                 raise ValueError("A object path is necessary in order to save results to Geddes")
         else:
-            logging.info("Skipping saving output in Geddes ...")
+            self.logger.info("Skipping saving output in Geddes ...")
 
-        df = self.executor.execute(flags)
-        if flags.save_to_geddes == True:
-            save_clusters_to_geddes(df, flags)
+        df = self.handler.handle(command)
+        if command.save_to_geddes == True:
+            save_clusters_to_geddes(df, command)
         return df
 
-class LocalDriveSaver(IAlgorithmExecutor):
-    def __init__(executor: IAlgorithmExecutor):
-        self.executor = executor
 
-    def execute(flags) -> pd.DataFrame:
-        df = self.executor.execute()
-        if flags.no_save_output == True:
-            logging.info("Skipping saving output locally...")
+class LocalDriveSaver(IExecuteAlgorithm):
+    def __init__(self, handler: IExecuteAlgorithm, logger: logging.Logger):
+        self.handler = handler
+        self.logger = logger
+
+    def handle(self, command) -> pd.DataFrame:
+        df = self.handler.handle(command)
+        if command.no_save_output == True:
+            self.logger.info("Skipping saving output locally...")
 
         return df
 
-class DatabaseSaver(IAlgorithmExecutor):
-    def __init__(executor: IAlgorithmExecutor):
-        self.executor = executor
 
-    def execute(flags) -> pd.DataFrame:
-        df = self.executor.execute(flags)
+class DatabaseSaver(IExecuteAlgorithm):
+    def __init__(self, handler: IExecuteAlgorithm, logger: logging.Logger):
+        self.handler = handler
+        self.logger = logger
+
+    def handle(self, command) -> pd.DataFrame:
+        df = self.handler.handle(command)
         return df
 
-class DisplayDf(IAlgorithmExecutor):
-    def __init__(executor: IAlgorithmExecutor):
-        self.executor = executor
 
-    def execute(flags) -> pd.DataFrame:
-        df = self.executor.execute(flags)
+class DisplayDf(IExecuteAlgorithm):
+    def __init__(self, handler: IExecuteAlgorithm, logger: logging.Logger):
+        self.handler = handler
+        self.logger = logger
+
+    def handle(self, command) -> pd.DataFrame:
+        df = self.handler.handle(command)
         print(df)
         return df
 
-class DataframeLogger(IAlgorithmExecutor):
-    def __init__(executor: IAlgorithmExecutor):
-        self.executor = executor
 
-    def execute(flags) -> pd.DataFrame:
-        df = self.executor.execute(flags)
+class DataframeLogger(IExecuteAlgorithm):
+    def __init__(self, handler: IExecuteAlgorithm, logger: logging.Logger):
+        self.handler = handler
+        self.logger = logger
+
+    def handle(self, command) -> pd.DataFrame:
+        df = self.handler.handle(command)
         self.logger.info(df)
         return df
--- nanoHUB/clustering/run_clustering.py
+++ nanoHUB/clustering/run_clustering.py
@@ -10,9 +10,13 @@ from preprocessing.gather_data import gather_data
 from core_classroom_detection.core_classroom_analysis import core_classroom_analysis
 from core_quick_cluster_detection.core_cost_cluster_analysis import core_cost_cluster_analysis, get_scratch_dir
 from save_clusters_to_geddes import save_clusters_to_geddes
+from algorithms_map import (
+    DateValidator, AlgorithmsMap, AlgorithmHandler, GeddesSaver, DisplayDf, DataframeLogger, ValidationHandler
+)
+
 
 @dataclass
-class ClusteringFlags:
+class ExecuteAlgorithmCommand:
     #general
     task: str
     start_date: str #datetimeformat: ####-##-##
@@ -59,79 +63,51 @@ class ClusteringFlags:
     def __post_init__(self):
         if not self.class_probe_range:
             self.class_probe_range = self.start_date + ":" + self.end_date
+
+        self.class_probe_range = self.class_probe_range.split(':')
+        self.data_probe_range = [datetime.strptime(x, '%Y-%m-%d') for x in self.class_probe_range]
+
         self.objectPath = 'clusters/${' + self.task + '}/by_semester'
-        self.validate()
-def cluster_by_semester(flags):
+        if not os.path.exists(get_scratch_dir(self)):
+            logging.info('Creating new scratch directory: ' + get_scratch_dir(self))
+        os.mkdir(get_scratch_dir(self))
+
+
+def cluster_by_semester(command):
     df_list = []
 
-    # start_dt = datetime.strptime(flags.start_date, '%Y-%m-%d')
+    # start_dt = datetime.strptime(command.start_date, '%Y-%m-%d')
     # sem1 = year + '-01-01'
     # sem2 = year + '-07-02'
-    yield run_clustering(flags)
-
-#flags -> ClusteringFlags
-def run_clustering(flags) -> pd.DataFrame:
+    yield run_clustering(command)
 
 
-    df = executor.execute(flags)
-
-    # add the loop
-    return df
-
-
-
-def maybe_save_raw_data(flags: ClusteringFlags) -> None:
-    if flags.use_old_data == False:
-        logging.info('Gathering data  ......')
-        gather_data(flags) #unsure how to handle with current solution
-    else:
-        logging.info('Option "--user_old_data" enabled. Using data from previous run ......')
-
-    logging.debug(pformat(vars(flags)))
+#command -> ClusteringFlags
+def run_clustering(command) -> pd.DataFrame:
+    handler = create_default_handler(command)
+    return handler.handle(command)
 
 
-
-def validate(self) -> None:
-    flags = self
-
-    # Algorithm Validation
-    if not hasattr(_map, flags.task):
-        raise Exception(
-            "Invalid Algorithm/Task. Valid algorithms/tasks are: %s" %_map
-        )
-
-    numeric_level = getattr(logging, flags.log_level.upper(), 10)
+def create_default_handler(command):
+    numeric_level = getattr(logging, command.log_level.upper(), 10)
     logging.basicConfig(level=numeric_level, format='%(message)s')
-        # summarize input options
+    _l = logging.getLogger()
 
-
-    #timeframe
-    if flags.class_probe_range == 'latest':
-        # probes only the latest (today - 2 STD of Gaussian attention window function)
-        # Each user simulation run action is expanded to 1 STD, and therefore the resulting cluster has max width of 2 STD
-        flags.data_probe_range = [datetime.date.today() - datetime.timedelta(days=flags.class_attention_span * 2),
-                                     datetime.date.today()]
-        flags.class_probe_range = [x.strftime("%Y-%m-%d") for x in flags.data_probe_range]
-
-    else:
-        # probes given time range
-        # expects inparams.class_probe_range in form of, for example, '2018-1-1:2018-5-1'
-        flags.class_probe_range = flags.class_probe_range.split(':')
-        flags.data_probe_range = [datetime.strptime(x, '%Y-%m-%d') for x in flags.class_probe_range]
-
-    # # create scratch directory if it does not exist
-    if not os.path.exists(get_scratch_dir(flags)):
-        logging.info('Creating new scratch directory: ' + get_scratch_dir(flags))
-        os.mkdir(get_scratch_dir(flags))
-
-def create_default_executor():
-    return DatabaseSaver(
+    return ValidationHandler(
+        [DateValidator()],
         GeddesSaver(
-            DisplayDf(AlgorithmExecutor(DataLoader(), AlgorithmsMap()))
-        )
+            DisplayDf(
+                DataframeLogger(
+                    AlgorithmHandler(
+                        AlgorithmsMap(), _l
+                    ), _l
+                ), _l
+            ), _l
+        ), _l
     )
 
 
 if __name__ == '__main__':
     #datetime parsing in alg file to view for checking enddate is later than startdate
-    run_clustering(ClusteringFlags('mike', '2006-01-01', '2007-07-02'))
+    command = ExecuteAlgorithmCommand('mike', '2006-01-01', '2007-07-02')
+    run_clustering(command)
