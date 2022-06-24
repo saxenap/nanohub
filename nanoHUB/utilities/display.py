06ed570b38eaf9e35eac224ac6a955136cb81fac
Praveen Saxena <saxenap@purdue.edu>
2022-06-23 21:40:14 -0400

    add display helpers

---
+++ nanoHUB/utilities/display.py
@@ -0,0 +1,29 @@
+# Created by saxenap at 6/16/22
+from pprint import pprint
+from inspect import getmembers
+from types import FunctionType
+
+
+class color:
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    DARKCYAN = '\033[36m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    END = '\033[0m'
+
+
+def attributes(obj):
+    disallowed_names = {
+        name for name, value in getmembers(type(obj))
+        if isinstance(value, FunctionType)}
+    return {
+        name: getattr(obj, name) for name in dir(obj)
+        if name[0] != '_' and name not in disallowed_names and hasattr(obj, name)}
+
+def print_attributes(obj):
+    pprint(attributes(obj))
