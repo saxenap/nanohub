a7663b5ebdeabe7d22d53af9bc4497942a3f3bfe
Praveen Saxena <saxenap@purdue.edu>
2022-10-25 01:36:12 -0400

    fix user entry

--- nanoHUB/pipeline/geddes/data_replication/computed_needs.py
+++ nanoHUB/pipeline/geddes/data_replication/computed_needs.py
@@ -15,7 +15,8 @@ from nanoHUB.pipeline.geddes.data import get_default_s3_client
 from nanoHUB.dataaccess.lake import S3FileMapper
 from nanoHUB.logger import logger as log
 from botocore.exceptions import ClientError
-
+import pymysql
+from sqlalchemy import exc
 
 application = Application.get_instance()
 nanohub_engine = application.new_db_engine('nanohub')
@@ -23,42 +24,81 @@ metrics_engine = application.new_db_engine('nanohub_metrics')
 s3_client = get_default_s3_client(application)
 # raw_mapper = S3FileMapper(s3_client, inparams.bucket_name)
 
-processed_mapper = S3FileMapper(s3_client, ClusteringConfiguration().bucket_name_processed)
+parser = argparse.ArgumentParser(
+    description='Designed for nanoHUB.org')
 
-# app_dir = os.environ['APP_DIR']
-# datadir = app_dir + '/.cache/'
-# file_path = datadir + 'registered_users.csv'
+parser.add_argument('--cache_dir', help='specific task', action='store')
+parser.add_argument('--chunk_size', help='specific task', action='store', type=int)
+parser.add_argument('--refresh_cache', help='specific task', action='store_true')
+parser.add_argument('--no_refresh_cache', dest='refresh_cache', action='store_false')
+parser.set_defaults(refresh_cache=True)
 
-sql_query = ''' 
+
+inparams = parser.parse_args()
+
+file_path = inparams.cache_dir.rstrip('/') + '/registered_users.csv'
+
+usernames_query_str = '''
 SELECT 
-    id, name, username, email, registerDate, lastvisitDate 
-FROM nanohub.jos_users
-WHERE username != ''
-AND username NOT IN ('gridstat', 'hubrepo')
+   DISTINCT user AS username
+FROM nanohub_metrics.toolevents events
+WHERE user != ''
+AND events.user NOT IN (SELECT username FROM ANALYTICS_derived_data.simulations)
+LIMIT {} OFFSET {}
+;
 '''
+# sql_query = '''
+# SELECT
+#     id, username
+# FROM nanohub.jos_users users
+# WHERE users.username != ''
+# AND users.username NOT IN ('gridstat', 'hubrepo')
+# AND users.username NOT REGEXP '^[-]'
+# AND users.id NOT IN (SELECT user_id FROM ANALYTICS_derived_data.simulations)
+# ORDER BY id
+# ;
+# '''
+# AND users.username NOT REGEXP '^[-]'
+# if inparams.refresh_cache == True:
+#     print("Refreshing cache from fresh user data.")
+#     event_users_df = pd.read_sql_query(sql_query_1, nanohub_engine)
+#     # print(users_df)
+#     print(len(event_users_df))
+#     event_users_df = event_users_df.reset_index()
+#     print("Writing users list to %s" % file_path)
+#     event_users_df.to_csv(file_path)
 
-users_df = pd.read_sql_query(sql_query, nanohub_engine)
-# print(users_df)
-print(len(users_df))
-users_df = users_df.reset_index()
-print(len(users_df))
-# users_df.to_csv(file_path)
+# sql_query_2 = '''
+# SELECT username FROM ANALYTICS_derived_data.simulations;
+# '''
+# sims_users_df = pd.read_sql_query(sql_query_2, nanohub_engine)
+#
+# df_merged = event_users_df.merge(sims_users_df.drop_duplicates(), on=['username'],
+#                    how='left', indicator=True)
+# df_not_incommon = df_merged.query("_merge == 'left_only'")
+# print(df_not_incommon)
+# print(len(df_not_incommon))
+
+# usernames_list = df_not_incommon['username'].tolist()
+# print(len(usernames_list))
+# print(df_not_incommon.tail())
 
 # users_df = pd.read_csv(file_path)
+# print(len(users_df))
 
 sql_query = """
-INSERT INTO ANALYTICS_derived_data.simulations
-(username,
- user_id,
- tool_count,
- session_count,
- sim_count,
- earliest_sim,
- latest_sim,
- active_duration,
- sim_lifetime,
- last_entryId,
- num_sim_active_days)
+    INSERT INTO ANALYTICS_derived_data.simulations
+    (username,
+     user_id,
+     tool_count,
+     session_count,
+     sim_count,
+     earliest_sim,
+     latest_sim,
+     active_duration,
+     sim_lifetime,
+     last_entryId,
+     num_sim_active_days)
 SELECT DISTINCT rollup.user AS username,
                 rollup.user_id AS user_id,
                 COUNT(DISTINCT rollup.tool_name) AS tool_count,
@@ -94,16 +134,45 @@ GROUP BY rollup.user
 ;
 """
 
-usernames_list = users_df['username'].tolist()
+# usernames_list = list(set(usernames_list))
+# print("Number of usernames: %d" % len(usernames_list))
+chunk_size = inparams.chunk_size
+offset = 0
+all_users = 0
+while True:
+    print("offset = %d" % offset)
+    usernames_query_formatted = usernames_query_str.format(chunk_size, offset)
+    print(usernames_query_formatted)
+    offset = offset + chunk_size
+    usernames_df = pd.read_sql(
+        usernames_query_formatted, nanohub_engine
+    )
+    usernames = usernames_df['username'].tolist()
 
-for i in range(0, len(usernames_list), 100):
-    usernames = usernames_list[i:i+100]
+    print(usernames)
+    if len(usernames) <= 0:
+        break
+
     placeholders = ','.join(['%s'] * len(usernames))
     sql_query = sql_query.format(users=placeholders)
-    # print(sql_query)
     with nanohub_engine.begin() as connection:
-        # print(tuple(usernames))
         connection.execute(sql_query, tuple(usernames))
-        print("%d users updated so far." % (i + len(usernames)))
+    all_users = all_users + len(usernames)
+    print("%d users updated" % len(usernames))
 
 
+# for i in range(0, len(usernames_list), chunk_size):
+#     usernames = usernames_list[i:i+chunk_size]
+#     placeholders = ','.join(['%s'] * len(usernames))
+#     sql_query = sql_query.format(users=placeholders)
+#     df = pd.read_sql(sql_query, nanohub_engine, tuple(usernames))
+#     # print(sql_query)
+#     with nanohub_engine.begin() as connection:
+#         print(tuple(usernames))
+#         print(i)
+#         print(i+chunk_size)
+#         connection.execute(sql_query, tuple(usernames))
+        # print(connection.execute("SELECT last_insert_id() FROM ANALYTICS_derived_data.simulations;"))
+
+    # print("%d existing users found." % existing_users)
+    # print("%d users updated so far." % (new_users))
